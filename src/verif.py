399c017e12a778a28b80462cdc551b9e674d5849
ExCLoudVision <86485185+ExCLoudVision@users.noreply.github.com>
2021-07-09 00:08:02 +0200

    Update verif.py

--- src/verif.py
+++ src/verif.py
@@ -2,39 +2,31 @@ from gtts import gTTS
 from io import BytesIO
 from discord import File
 from random import choice
-from string import ascii_uppercase, digits
+from string import ascii_lowercase, digits
 from asyncio import sleep
-
+import time
+codeuser = ""
 chars = []
-
-
-"""
-for a in ascii_uppercase:
+for a in ascii_lowercase:
     chars.append(a)
-"""    
-
-for a in digits:
-    chars.append(a)
-
-
-
-
+def generate_code():
+    buf = ""
+    for x in range(6):
+        buf += choice(ascii_lowercase)
+        buf += "."
+    return buf
 async def code_verify(channel):
-
-    code = "".join(choice(chars) for _ in range(6))
-
-    print(code)
-
+    code = generate_code()
     f = BytesIO()
-
     code_file = gTTS(text=code.lower())
-
     code_file.write_to_fp(f)
-
     f.seek(0)
-
     fichier_say = File(f, "code.mp3")
-
-    await channel.send(file=fichier_say)
-
-    return code
+    msg = await channel.send(file=fichier_say)
+    codeuser = code.replace(".", "")
+    print(codeuser)
+    await sleep(30)
+    await msg.delete()
+    return code.replace(".","")
+def get_code():
+    return codeuser
