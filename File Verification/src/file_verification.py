e2c031acfe0f57f42476b467e561c7ab3dc2674e
Billy <77754159+billythegoat356@users.noreply.github.com>
2021-07-13 20:45:44 +0200

    Update file_verification.py

--- File Verification/src/file_verification.py
+++ File Verification/src/file_verification.py
@@ -9,12 +9,11 @@ async def file_verification(file, author, key, max_size, user=None):
 
         if user:
             
-            if max_size:
-                if file.size > max_size:
-                    await user.send(content=f"Fichier suspect envoyé de la part de {author.mention}! Mais le fichier dépassait la taille maximum autorisée de {max_size} avec une taille de {file.size}!")
+            if max_size and file.size > max_size:
+                await user.send(content=f"Fichier suspect envoyé de la part de {author.mention}! Mais le fichier dépassait la taille maximum autorisée de {max_size} avec une taille de {file.size}!")
 
-            else:
-                await user.send(content=f"Fichier suspect envoyé de la part de {author.mention}!", file=await file.to_file())
+            await user.send(content=f"Fichier suspect envoyé de la part de {author.mention}!", file=await file.to_file())
+
         
         return True
     
